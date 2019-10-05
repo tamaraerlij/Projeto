1f202b4f36432c310545a26f4809905e18fd4229
Tamara <tamara.erlij@gmail.com>
2019-10-05 16:36:15 -0300

    Modelo basico de eventos e categorias

---
+++ Ecosonder/Categorias.swift
@@ -0,0 +1,37 @@
+//
+//  Categorias.swift
+//  Ecosonder
+//
+//  Created by Tamara Erlij on 04/10/19.
+//  Copyright © 2019 Tamara Erlij. All rights reserved.
+//
+
+import SwiftUI
+
+struct Categorias: View {
+    
+    var body: some View {
+       
+//        CategoriasView(title: "Energia", color: .yellow)
+//        CategoriasView(title: "Moda", color: .pink)
+//
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct Categorias_Previews: PreviewProvider {
+    static var previews: some View {
+     
+    }
+}
--- Ecosonder/ContentView.swift
+++ Ecosonder/ContentView.swift
@@ -7,49 +7,60 @@
 
 import SwiftUI
 
-enum DragInfo {
-    case inactive
-    case active(translation: CGSize)
-    
-    var translation: CGSize {
-        switch self{
-        case .inactive:
-            return .zero
-        case .active(let t):
-            return t
-        }
-    }
-    var isActive: Bool {
-    switch self {
-    case .inactive: return false
-    case .active: return true
-}
-    }
-}
 struct ContentView : View {
-    @GestureState var dragInfo = DragInfo.inactive
+    
+  let categorias = ["1", "2", "3", "4", "5"]
+    
+ @State var expanded: Bool = false
  
     var body: some View {
-        let gesture = DragGesture()
-              .updating($dragInfo) { (value, dragInfo, _) in
-                  dragInfo = .active(translation: value.translation)
-              }
 
-      return ZStack {
-            CardView(title: "Evento sobre sustentabilidade", color: .green)
-                .offset(x: 0, y: dragInfo.isActive ? -400 : -40)
-                .scaleEffect(dragInfo.isActive ? 1 : 0.90)
-            CardView(title: "Venham", color: .purple)
-                .offset(x: 0, y: dragInfo.isActive ? -200 : -20)
-                .scaleEffect(dragInfo.isActive ? 1 : 0.95)
-            CardView(title: "Ecosonder", color: .black)
-                 .offset(dragInfo.translation)
-                 .gesture(gesture)
-            }
-            .offset(x: 0, y: 80)
-            .animation(.spring())
+//    ZStack {
+//            CardView(title: "Evento sobre sustentabilidade", color: .green)
+//                .offset(x: 0, y: expanded ? -400 : -40)
+//                .scaleEffect(expanded ? 1 : 0.90)
+//            CardView(title: "Venham", color: .purple)
+//                .offset(x: 50, y: expanded ? -400 : -40)
+//                .scaleEffect(expanded ? 1 : 0.95)
+//            CardView(title: "Ecosonder", color: .black)
+//                .onTapGesture {
+//                    self.expanded.toggle()
+//            }
+//           .offset(x: 100, y: 80)
+//            .animation(.spring())
+         //   Spacer()
+          NavigationView {
+            List {
+                
+                ScrollView {
+                    VStack (alignment: .leading) {
+                        Text("Eventos principais")
+                    HStack {
+                          Text("Evento 1")
+                          Text("Evento 2")
+                          Text("Evento 3")
+                          Text("Evento 4")
+                          Text("Evento 5")
+                          Text("Evento 6")
+                          Text("Evento 7")
+                          Text("Evento 8")
+                          Text("Evento 9")
+                    }
+                    }
+                }.frame(height: 150)
+                ForEach(categorias,  id: \.self) { categoria in
+                        Text(categoria)
+
+                }
+//                CategoriasView(title: "Energia", color: .yellow)
+//                CategoriasView(title: "Moda", color: .pink)
+            } .navigationBarTitle(Text("Grupos"))
+        }
+ 
+        }
     }
-}
+//}
+
 
 struct CardView : View {
     
@@ -61,7 +72,28 @@ struct CardView : View {
             Rectangle()
                 .fill(color)
                 .cornerRadius(10)
-                .frame(width: 320, height: 320)
+                .frame(width: 350, height: 300)
+            
+            Text(title)
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.white)
+        }.shadow(radius: 6)
+    }
+}
+
+
+struct CategoriasView : View {
+    
+    let title : String
+    let color : Color
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(color)
+                .cornerRadius(10)
+                .frame(width: 400, height: 50)
             
             Text(title)
                 .font(.largeTitle)
@@ -75,7 +107,6 @@ struct CardView : View {
 
 
 
-
 #if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -83,3 +114,7 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 #endif
+
+//
+//CategoriasView(title: "Energia", color: .yellow)
+//CategoriasView(title: "Moda", color: .pink)
